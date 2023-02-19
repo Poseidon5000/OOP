dbd5e3bf83c39c65127ffa61af47ca49246123bd
Poseidon5000 <okonnsikangoodnews@gmail.com>
2023-02-19 16:15:10 +0100

    prototype inheritance

--- oop.js
+++ oop.js
@@ -1,16 +1,28 @@
 'use strict'
 // ABSTRACTION, ENCAPULSATION, INHERITANCE, POLYMORPHISM.
 
+//Creating objects in OOP (blueprints to which other object is created)
+
 function person (firstName, birthYear){
     this.firstName = firstName;
     this.birthYear = birthYear
 }
+const poseidon = new person("Okon", 1990);
+
+//USING PROTOTYPES
 
 person.prototype.calcAge = function(){
   return 2023- this.birthYear;
 }
 
-const poseidon = new person("Okon", 1990);
+person.prototype.species = "homo sapiens"
+// console.log(poseidon.__proto__ === person.prototype);
+// console.log(poseidon.__proto__);
+// console.log(person.prototype.isPrototypeOf(poseidon));
+
+console.log(poseidon.species);
+
+
 console.log(poseidon.calcAge())
 
 
